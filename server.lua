6b4052724f39f3f10c00b4e91b161ae46f226e9a
Tony Stark <76168122+tony-stark-17@users.noreply.github.com>
2022-03-30 19:28:16 +0530

    QBCore Compatibility

--- server.lua
+++ server.lua
@@ -1,13 +1,28 @@
+if Config.ESX then
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+elseif Config.QB then
+    QBCore =  exports['qb-core']:GetCoreObject()
+end
 
-RegisterServerEvent('setJob', function(job) 
-    local xPlayer = ESX.GetPlayerFromId(source)
+RegisterServerEvent('setJob', function(job)
+    local Player = nil
+    if Config.ESX then
+          Player = ESX.GetPlayerFromId(source) 
+    elseif Config.QB then
+          Player = QBCore.Functions.GetPlayer(source) 
+    end
     for k, v in pairs(Config.Jobs) do
         if v == job then
-            xPlayer.setJob(job, 1)
-            print('Successfully set jobbed'..xPlayer.getName())
+            if Config.ESX then
+            xPlayer.setJob(job, 0)
+            print('Successfully set job: '..xPlayer.getName())
             xPlayer.showNotification('Hey, you have a new job, '..job)
+            elseif Config.QB then
+                local JobInfo = QBCore.Shared.Jobs[job]
+                Player.Functions.SetJob(job, 0)
+                TriggerClientEvent('QBCore:Notify', src, 'Congratulations with your new job! ('..JobInfo.label..')')
+            end
         end
     end
 end)
