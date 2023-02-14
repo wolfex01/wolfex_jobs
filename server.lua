bb2cd8c81bea080a89d7bb021b942eec51e5e036
wolfex <91018450+wolfex01@users.noreply.github.com>
2023-02-14 21:25:18 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,13 +1,16 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('setJob', function(job) 
-    local xPlayer = ESX.GetPlayerFromId(source)
-    for k, v in pairs(Config.Jobs) do
-        if v == job then
-            xPlayer.setJob(job, 1)
-            print('Successfully set jobbed'..xPlayer.getName())
-            xPlayer.showNotification('Hey, you have a new job, '..job)
+RegisterServerEvent('setJob', function(job)
+    local xPlayer = ESX.GetPlayerFromId(source) -- lekérjük a játékos objektumot az azonosító alapján
+
+    -- végigmegyünk a Config.Jobs táblán, és beállítjuk a munkát, ha megtaláljuk azt
+    for _, jobName in ipairs(Config.Jobs) do
+        if jobName == job then
+            xPlayer.setJob(job, 1) -- beállítjuk a játékos munkáját
+            print('Successfully set job for ' .. xPlayer.getName()) -- naplózzuk az eseményt
+            xPlayer.showNotification('Hey, you have a new job, ' .. job) -- küldünk egy értesítést a játékosnak a munka beállításáról
+            break -- kilépünk a ciklusból, ha megtaláltuk a munkát
         end
     end
 end)
