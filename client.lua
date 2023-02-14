d696dcc2f21050fdaab92e082303adc11ec74aa8
wolfex <91018450+wolfex01@users.noreply.github.com>
2023-02-14 21:24:04 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,34 +1,42 @@
-local cursorState = false
-RegisterNUICallback('setJob', function(data) 
-    cursorState = not cursorState
-    SendNUIMessage({
-        type = 'job'
-    })
-    SetNuiFocus(cursorState,cursorState)
-    TriggerServerEvent('setJob', data.job)
+local cursorState = false -- inicializáljuk a kurzor állapotát, és beállítjuk, hogy kezdetben inaktív
+local jobCenterMarker = 25 -- egy konstans érték, amely a DrawMarker függvény marker típusának megadására szolgál
+local jobCenterRadius = 5.0 -- a munkaközpont marker észleléséhez használt hatósugár
+
+-- a "setJob" NUI callback függvény, amelyet a kliens oldalon hívunk meg a munka beállítására
+RegisterNUICallback('setJob', function(data)
+    cursorState = not cursorState -- a kurzor állapotának megváltoztatása
+    SetNuiFocus(cursorState, cursorState) -- a NUI fókusz beállítása az aktuális kurzor állapot szerint
+    SendNUIMessage({ type = 'job' }) -- elküldjük az üzenetet az NUI felületnek a "job" típussal
+    TriggerServerEvent('setJob', data.job) -- elküldjük a szervernek a "setJob" eseményt adatainkkal
 end)
 
-local openJobcenter = function() 
-    cursorState = not cursorState
-    SetNuiFocus(cursorState, cursorState)
-    SendNUIMessage({
-        type = 'job',
-        jobs = Config.Jobs
-    })
-end    
+-- a "job center" megnyitása
+local openJobCenter = function()
+    cursorState = not cursorState -- a kurzor állapotának megváltoztatása
+    SetNuiFocus(cursorState, cursorState) -- a NUI fókusz beállítása az aktuális kurzor állapot szerint
+    SendNUIMessage({ type = 'job', jobs = Config.Jobs }) -- elküldjük az üzenetet az NUI felületnek a "job" és "jobs" típusokkal
+end
 
-CreateThread(function() 
-    while true do 
+-- fő szál
+Citizen.CreateThread(function()
+    while true do
         Citizen.Wait(5)
-    local myCoords = GetEntityCoords(PlayerPedId())
-    for k,v in ipairs(Config.JobCoords) do
-        if GetDistanceBetweenCoords(myCoords.x, myCoords.y, myCoords.z, v[1].x, v[1].y, v[1].z) < 5 then
-            DrawMarker(25, v[1].x, v[1].y, v[1].z -1, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 255, 128, 0, 50, false, true, 2, nil, nil, false)
-                if IsControlJustPressed(0,38) then
-                    openJobcenter()
-                end
+
+        local myCoords = GetEntityCoords(PlayerPedId())
+
+        -- végigmegyünk a Config.JobCoords tömbön, és megjelenítjük a munkaközpontokat a játékban
+        for _, jobCoord in ipairs(Config.JobCoords) do
+            local jobCoordDistance = GetDistanceBetweenCoords(myCoords, jobCoord[1], true)
+
+            -- ha a játékos elég közel van a markerhez, akkor megjelenítjük azt, és ha megnyomja az F kulcsot, akkor megnyitjuk a "job center"-t
+            if jobCoordDistance <= jobCenterRadius then
+                DrawMarker(jobCenterMarker, jobCoord[1], 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.0, 1.0, 1.0, 255, 128, 0, 50, false, true, 2, nil, nil, false)
+
+            if IsControlJustPressed(0, 38) then
+                 openJobCenter()
+             end
             end
-        end        
+        end
     end
 end)
 
